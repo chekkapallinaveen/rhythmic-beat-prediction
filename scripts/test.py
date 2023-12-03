95fbf0c4b46e8d9057d67060c3dff35c5ecc4bad
chekkapallinaveen <chekkapalli.naveen@students.iiit.ac.in>
2023-12-03 23:59:43 +0530

    added cnn_model dump

--- scripts/test.py
+++ scripts/test.py
@@ -2,6 +2,11 @@
 # output the predicted label
 
 import pickle
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from joblib import load
 
 in_song = input("Enter the path to the audio file: ")
 
@@ -13,13 +18,12 @@ in_song = input("Enter the path to the audio file: ")
 #     return song
 
 # data processing and feature extraction
+
 import librosa
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 def extract_features(file_path, duration_per_step=0.1, n_fft=1024, n_mfcc=13, hop_length=512):
-  
-  print("Extracting features from {}".format(file_path))
-  
   amplitudes = []
   magnitude_spectrograms = []
   mfcc_features = []
@@ -79,11 +83,181 @@ chromas_new = np.mean(chromas_new, axis=1)
 features = np.hstack((amps, mags, mfccs_new, chromas_new))
 
 features = np.array(features).reshape(1, -1)
-print(features.shape)
 
 import pickle
 
 with open('/Users/chnaveen/Documents/sem5/music/rhythmic-beat-prediction/scripts/dt_amp_mag_classifier.pkl', 'rb') as f:
   loaded_dt_model = pickle.load(f)
+  
+with open('/Users/chnaveen/Documents/sem5/music/rhythmic-beat-prediction/scripts/label_encoder.pkl', 'rb') as f:
+  lc = pickle.load(f)
 
-print(loaded_dt_model.predict(features))
+j_model = load('/Users/chnaveen/Documents/sem5/music/rhythmic-beat-prediction/scripts/dt_amp_mag_classifier.joblib')
+
+print(lc.inverse_transform(loaded_dt_model.predict(features)))
+print(lc.inverse_transform(j_model.predict(features)))
+
+
+class Reshape(nn.Module):
+  def __init__(self, shape):
+    super(Reshape, self).__init__()
+    self.shape = shape
+
+  def forward(self, x):
+    return x.view(self.shape)
+
+class CNN(nn.Module):
+  def __init__(
+    self,
+    kernel_size=3,
+    stride=1,
+    padding=1,
+    dropout=0.2,
+    learning_rate=0.001,
+    batch_size=64,
+    num_epochs=5
+  ):
+    super(CNN, self).__init__()
+    
+    self.conv1 = nn.Conv2d(
+      in_channels=1, 
+      out_channels=16, 
+      kernel_size=kernel_size, 
+      stride=stride,
+      padding=padding
+    )
+    self.relu = nn.ReLU()
+    self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+    
+    self.conv2 = nn.Conv2d(
+      in_channels=16, 
+      out_channels=32,
+      kernel_size=kernel_size,
+      stride=stride,
+      padding=padding
+    )
+    
+    self.dim1 = 10
+    self.dim2 = 30
+    
+    self.adjust_shape = Reshape((-1, 32*self.dim1*self.dim2))
+    
+    self.dropout = nn.Dropout(p=dropout)
+    self.fc = nn.Linear(32*self.dim1*self.dim2, 4)
+    self.softmax = nn.Softmax(dim=1)
+    
+    self.layers = [
+      self.conv1,         
+      self.pool,          
+      self.relu,          
+      self.conv2,         
+      self.pool,          
+      self.relu,          
+      self.dropout,       
+      self.adjust_shape,  
+      self.fc,            
+      self.softmax        
+    ]
+    
+    self.criterion = nn.CrossEntropyLoss()
+    self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+    self.num_epochs = num_epochs
+    self.batch_size = batch_size
+
+  def forward(self, x):
+    x = self.conv1(x)
+    x = self.pool(x)
+    x = self.relu(x)
+    
+    x = self.conv2(x)
+    x = self.pool(x)
+    x = self.relu(x)
+    
+    x = self.dropout(x)
+    x = self.adjust_shape(x)
+    x = self.fc(x)
+    x = self.softmax(x)
+    return x
+  
+  def train_model(self, train_dataset, val_dataset):
+    train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=self.batch_size, shuffle=True)
+    
+    train_loss, val_loss, accuracy, val_accuracy = 0,0,0,0
+    
+    for epoch in range(self.num_epochs):
+      self.train()
+      train_loss = 0.0
+      correct = 0
+      total = 0
+      
+      val_loss = 0.0
+      val_correct = 0
+      val_total = 0
+      
+      for inputs, labels in train_loader:
+        self.optimizer.zero_grad()
+        outputs = self(inputs)
+        loss = self.criterion(outputs, labels)
+        loss.backward(retain_graph=True)
+        self.optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+      self.eval()
+      for inputs, labels in val_loader:
+        self.optimizer.zero_grad()
+        outputs = self(inputs)
+        loss = self.criterion(outputs, labels)
+        loss.backward(retain_graph=True)
+        self.optimizer.step()
+
+        val_loss += loss.item()
+        _, predicted = torch.max(outputs.data, 1)
+        val_total += labels.size(0)
+        val_correct += (predicted == labels).sum().item()
+
+      val_accuracy = val_correct / val_total
+      accuracy = correct / total
+
+      print(
+        f'Epoch {epoch+1}/{self.num_epochs}, '
+        f'Loss(Train): {train_loss:.4f}, '
+        f'Accuracy(Train): {accuracy:.2f}, '
+        f'Loss(Val): {val_loss:.4f}, '
+        f'Accuracy(Val): {val_accuracy:.2f}'
+      )
+    return train_loss, val_loss, accuracy, val_accuracy
+  
+  def predict(self, pred_dataset):
+    pred_loader = DataLoader(pred_dataset, batch_size=self.batch_size, shuffle=False)
+    self.eval()
+    predictions = []
+    labels_true = []
+    for inputs, labels in pred_loader:
+      outputs = self(inputs)
+      _, predicted = torch.max(outputs.data, 1)
+      predictions.extend(predicted.tolist())
+      labels_true.extend(labels.numpy())
+    return predictions, labels_true
+  
+  def predict_without_labels(self, pred_dataset):
+    pred_loader = DataLoader(pred_dataset, batch_size=self.batch_size, shuffle=False)
+    self.eval()
+    predictions = []
+    for inputs in pred_loader:
+      outputs = self(inputs)
+      _, predicted = torch.max(outputs.data, 1)
+      predictions.extend(predicted.tolist())
+    return predictions
+  
+cnn_model = CNN()
+cnn_model.load_state_dict(torch.load('/Users/chnaveen/Documents/sem5/music/rhythmic-beat-prediction/scripts/cnn_model.pt'))
+
+cnn_features = np.array(features).reshape(1, 1, 40, 120)
+cnn_model.eval()
+preds = cnn_model.predict_without_labels(cnn_features)
+print(lc.inverse_transform(preds))
